c91aedfcf4640238d5ea2e5c522b3675c70df1b0
Brenton Ashworth <bashwort@gmail.com>
2010-03-23 16:06:22 -0700

    Switched to Clojure 1.2. README update.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject sandbar "0.0.1-SNAPSHOT"
   :description "Clojure web application library."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
                  [compojure "0.3.2"]
                  [mysql/mysql-connector-java "5.1.6"]]
   :namespaces [sandbar.database
                sandbar.library
-               sandbar.security])
+               sandbar.auth
+               sandbar.userui])
