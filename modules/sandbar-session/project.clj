cd7bb1a8ad844dbef623a76353a6529ed3ce1f67
Allen Rohner <arohner@gmail.com>
2010-07-03 15:34:27 -0500

    Bump clojure version, don't AOT to improve interop

--- modules/sandbar-session/project.clj
+++ modules/sandbar-session/project.clj
@@ -1,9 +1,8 @@
 (defproject sandbar/sandbar-session "0.2.3"
   :description "Easier session management for Ring."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [ring/ring-core "0.2.3"]
                  [sandbar/sandbar-core "0.3.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [jline "0.9.94"]]
-  :namespaces [sandbar.stateful-session])
+                     [jline "0.9.94"]])
